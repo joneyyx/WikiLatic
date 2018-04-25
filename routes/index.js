dc87d76f7d980b0b450b8fe12f44cc27fe7e3a09
ZYCGary <zyc.gary@gmail.com>
2018-04-25 21:29:56 +1000

    Add signin views

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,25 @@ let express = require('express');
 let router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+
+router.get('/login', function (req, res, next) {
+    res.render('login');
+});
+
+router.get('/logout', function (req, res, next) {
+    res.redirect('index');
+});
+
+router.get('/signup', function (req, res, next) {
+    res.render('sign-up');
+});
+
+router.post('/signup', function (req, res, next) {
+
 });
 
 module.exports = router;
+
